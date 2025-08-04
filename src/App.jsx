e5471cb5c6470111bd43f4b3d8e3b5fe519b706f
kominjung04 <heppy8888@naver.com>
2025-08-05 00:51:19 +0900

    perf:시간 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const[time, setTime] = useState('');
+
+  useEffect(() => {
+    setTime(updateTime());
+
+    const interval = setInterval(() => {
+      setTime(updateTime());
+    },1000);
+
+    return () => clearInterval(interval);
+  },[time]);
 
   return (
     <>
+    <div className='App'>
+      <div className='time'>
+        {updateTime()}
+      </div>
     <h1>ToDoList</h1>
     <div class="input">
     <input placeholder='할 일을 입력하시오.'></input>  <button>+</button>
     </div>
+    </div>
     </>
   )
+}
+
+function updateTime(){
+  const now = new Date();
+  // 현재 시간 가져오기
+  // padStart를 사용하여 두 자리 숫자로 표시
+  const hours = String(now.getHours()).padStart(2, '0'); // 시간
+  const minutes = String(now.getMinutes()).padStart(2, '0'); // 분
+  const second = String(now.getSeconds()).padStart(2, '0'); // 초
+  return `${hours}:${minutes}:${second}`;
+    
 }
 
 export default App
