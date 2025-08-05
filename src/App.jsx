a7c1a35521a467c73f4d753de3706a58b5aa399b
kominjung04 <heppy8888@naver.com>
2025-08-06 02:10:13 +0900

    perf:날짜 추가가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   const[time, setTime] = useState('');
+  const[date,setDate] = useState('');
+
 
   useEffect(() => {
     setTime(updateTime());
@@ -16,24 +18,39 @@ function App() {
     return () => clearInterval(interval);
   },[time]);
 
+  useEffect(() => {
+    setTime(updateDate());
+
+    // setInterval: 일정한 간격마다 함수 반복 실행
+    // 1초마다 업데이트 
+    const interval = setInterval(() => {
+      setTime(updateDate());
+    },1000);
+
+    return () => clearInterval(interval);
+  },[date]);
+
   return (
     <>
     <div className='App'>
       <div className='time'>
         {updateTime()}
       </div>
-    <h1>ToDoList</h1>
     <div class="input">
+    <h1>ToDoList</h1>
     <input placeholder='할 일을 입력하시오.'></input>  <button>+</button>
+    </div>
+    <div className='date'>
+      {updateDate()}
     </div>
     </div>
     </>
   )
 }
 
+  // 현재 시간 가져오기
 function updateTime(){
   const now = new Date();
-  // 현재 시간 가져오기
   // padStart를 사용하여 두 자리 숫자로 표시
   const hours = String(now.getHours()).padStart(2, '0'); // 시간
   const minutes = String(now.getMinutes()).padStart(2, '0'); // 분
@@ -42,4 +59,13 @@ function updateTime(){
     
 }
 
+// 현재 날짜 가져오기
+function updateDate(){
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth()).padStart(2, '0');
+  const day = String(now.getDay()).padStart(2,'0');
+  
+  return `${year}-${month}-${day}`;
+}
 export default App
